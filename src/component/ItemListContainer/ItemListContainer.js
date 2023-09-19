6e6cfc076364fd4654bb6061f049171ae161bf0b
Evelina Niemann <evelinaniemann@gmail.com>
2023-09-19 10:47:48 -0300

    Category in ItemListContainer

--- src/component/ItemListContainer/ItemListContainer.js
+++ src/component/ItemListContainer/ItemListContainer.js
@@ -1,31 +1,38 @@
-import { useState, useEffect } from "react";
-import data from "../../data/data.json";
+import React, { useState, useEffect } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { getProducts, categoryId } from "../../PedirDatos/PedirDatos";
+import "./styles.css";
 import Loading from "../Loading/Loading";
 import ItemList from "../ItemList/ItemList";
 
 const ItemListContainer = ({ greeting }) => {
   const [loading, setLoading] = useState(false);
   const [courses, setCourses] = useState([]);
-
-  const orderProduct = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(data);
-      }, 1500);
-    });
-  };
+  const { category } = useParams();
 
   useEffect(() => {
     setLoading(true);
-    orderProduct()
-      .then((res) => {
+    const asyncFunc = async () => {
+      try {
+        const res = await (category ? categoryId(category) : getProducts());
         setCourses(res);
-      })
-      .finally(() => setLoading(false));
-  }, []);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    asyncFunc();
+  }, [category]);
+
   return (
     <>
       <h2>{greeting}</h2>
+      <nav className="tabs">
+        <NavLink to={`/cursos`}>Todos</NavLink>
+        <NavLink to={`/category/producto`}>Producto</NavLink>
+        <NavLink to={`/category/programacion`}>Programación</NavLink>
+      </nav>
+
       {loading ? <Loading /> : <ItemList courses={courses} loading={loading} />}
     </>
   );
